78b8f87d6686804ee1c1ddd6d35709d6341f3274
jrnold <jeffrey.arnold@gmail.com>
2019-01-15 23:04:42 -0800

    initial commit

---
+++ share-this-config.js
@@ -0,0 +1,14 @@
+// settings for ShareThis
+const shareThis = window.ShareThis;
+const twitterSharer = window.ShareThisViaTwitter;
+
+// set this to your
+const githubRepo = "jrnold/r4ds-exercise-solutions";
+// optionally add a github label to use
+const githubIssueLabel = null;
+const githubSharer = new GitHubIssueSharer(githubRepo, label = githubIssueLabel);
+
+const selectionShare = shareThis({
+    sharers: [ githubSharer, twitterSharer ]
+});
+selectionShare.init();
